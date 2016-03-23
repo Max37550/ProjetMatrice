cbc0b8b48022f63020ea7836c188f0011a3fa6d0
Max37550 <maxime.peralez@hotmail.fr>
2016-03-23 17:42:07 +0100

    Added files via upload

--- Ctableau2D.cpp
+++ Ctableau2D.cpp
@@ -7,3 +7,84 @@
 //
 
 #include "Ctableau2D.hpp"
+
+template <class T>
+Ctableau2D<T>::Ctableau2D(){
+    uiT2DNbColonnes = 0;
+    uiT2DNbLignes = 0;
+    pT2DContenu = NULL;
+}
+
+template <class T>
+Ctableau2D<T>::Ctableau2D(Ctableau2D<T> &T2DTab){
+    uiT2DNbLignes = T2DTab.T2DLireNbLignes();
+    uiT2DNbColonnes = T2DTab.T2DLireNbColonnes();
+    
+    pT2DContenu = new T*[uiT2DNbLignes];
+    for(unsigned int uiLigne = 0; uiLigne < uiT2DNbLignes; uiLigne++){
+        pT2DContenu[uiLigne] = new T[uiT2DNbColonnes];
+        for(unsigned int uiColonne =0; uiColonne <= uiT2DNbColonnes; uiColonne++){
+            pT2DContenu[uiLigne][uiColonne] = T2DTab.T2DLireContenu()[uiLigne][uiColonne];
+        }
+    }
+}
+
+template <class T>
+Ctableau2D<T>::Ctableau2D(unsigned int nbLignes, unsigned int nbColonnes,T **pContenu){
+    uiT2DNbLignes = nbLignes;
+    uiT2DNbColonnes = nbColonnes;
+    
+    pT2DContenu = new T*[nbLignes];
+    for(unsigned int uiLigne = 0; uiLigne < nbLignes; uiLigne++){
+        pT2DContenu[uiLigne] = new T[nbColonnes];
+        for(unsigned int uiColonne =0; uiColonne <= nbColonnes; uiColonne++){
+            pT2DContenu[uiLigne][uiColonne] = pContenu[uiLigne][uiColonne];
+        }
+    }
+}
+
+template <class T>
+Ctableau2D<T>::~Ctableau2D(){
+    
+}
+
+template <class T>
+unsigned int Ctableau2D<T>::T2DLireNbLignes(){
+    return uiT2DNbLignes;
+}
+
+template <class T>
+unsigned int Ctableau2D<T>::T2DLireNbColonnes(){
+    return uiT2DNbColonnes;
+}
+
+template <class T>
+T** Ctableau2D<T>::T2DLireContenu(){
+    return pT2DContenu;
+}
+
+template <class T>
+void Ctableau2D<T>::T2DAffichage(){
+    for(int i = 0;i<uiT2DNbLignes;i++){
+        for(int j = 0;j<uiT2DNbColonnes;j++){
+            std::cout << pT2DContenu[i][j] << '\t';
+        }
+        std::cout <<std::endl;
+    }
+}
+
+template <class T>
+Ctableau2D<T> Ctableau2D<T>::operator=(Ctableau2D<T> &T2DTab){
+    uiT2DNbLignes = T2DTab.T2DLireNbLignes();
+    uiT2DNbColonnes = T2DTab.T2DLireNbColonnes();
+    
+    pT2DContenu = new T*[uiT2DNbLignes];
+    for(unsigned int uiLigne = 0; uiLigne < uiT2DNbLignes; uiLigne++){
+        pT2DContenu[uiLigne] = new T[uiT2DNbColonnes];
+        for(unsigned int uiColonne = 0; uiColonne <= uiT2DNbColonnes; uiColonne++){
+            pT2DContenu[uiLigne][uiColonne] = T2DTab.T2DLireContenu()[uiLigne][uiColonne];
+        }
+    }
+    return *this;
+}
+
--- Ctableau2D.hpp
+++ Ctableau2D.hpp
@@ -11,6 +11,7 @@
 
 #include <iostream>
 #include <stdio.h>
+
 template <class T>
 class Ctableau2D {
     //Attributs
@@ -18,68 +19,24 @@ class Ctableau2D {
     unsigned int uiT2DNbLignes;
     unsigned int uiT2DNbColonnes;
     T **pT2DContenu;
+    
     //Constructeurs et destructeur
     public :
-    Ctableau2D(){
-        uiT2DNbColonnes = 0;
-        uiT2DNbLignes = 0;
-        pT2DContenu = NULL;
-    }
-    Ctableau2D<T>(Ctableau2D<T> &T2DTab){
-        uiT2DNbLignes = T2DTab.T2DLireNbLignes();
-        uiT2DNbColonnes = T2DTab.T2DLireNbColonnes();
-        
-        pT2DContenu = new T*[uiT2DNbLignes];
-        for(unsigned int uiLigne = 0; uiLigne < uiT2DNbLignes; uiLigne++){
-            pT2DContenu[uiLigne] = new T[uiT2DNbColonnes];
-            for(unsigned int uiColonne =0; uiColonne <= uiT2DNbColonnes; uiColonne++){
-                pT2DContenu[uiLigne][uiColonne] = T2DTab.T2DLireContenu()[uiLigne][uiColonne];
-            }
-        }
-    }
-    Ctableau2D<T>(unsigned int nbLignes, unsigned int nbColonnes,T **pContenu){
-        uiT2DNbLignes = nbLignes;
-        uiT2DNbColonnes = nbColonnes;
-        
-        pT2DContenu = new T*[nbLignes];
-        for(unsigned int uiLigne = 0; uiLigne < nbLignes; uiLigne++){
-            pT2DContenu[uiLigne] = new T[nbColonnes];
-            for(unsigned int uiColonne =0; uiColonne <= nbColonnes; uiColonne++){
-                pT2DContenu[uiLigne][uiColonne] = pContenu[uiLigne][uiColonne];
-            }
-        }
-    }
-    ~Ctableau2D<T>(){}
+    Ctableau2D<T>();
+    Ctableau2D<T>(Ctableau2D<T> &T2DTab);
+    Ctableau2D<T>(unsigned int nbLignes, unsigned int nbColonnes,T **pContenu);
+    ~Ctableau2D<T>();
+    
     //Méthodes
     public :
-    unsigned int T2DLireNbLignes(){return uiT2DNbLignes;}
-    unsigned int T2DLireNbColonnes(){return uiT2DNbColonnes;}
-    T** T2DLireContenu(){return pT2DContenu;}
-    void T2DAffichage(){
-        for(int i = 0;i<uiT2DNbLignes;i++){
-            for(int j = 0;j<uiT2DNbColonnes;j++){
-                std::cout << pT2DContenu[i][j];
-            }
-            std::cout <<std::endl;
-        }
-    }
+    unsigned int T2DLireNbLignes();
+    unsigned int T2DLireNbColonnes();
+    T** T2DLireContenu();
+    void T2DAffichage();
     
     //Surcharge
     public :
-    Ctableau2D<T> operator=(Ctableau2D<T> &T2DTab){
-        uiT2DNbLignes = T2DTab.T2DLireNbLignes();
-        uiT2DNbColonnes = T2DTab.T2DLireNbColonnes();
-        
-        pT2DContenu = new T*[uiT2DNbLignes];
-        for(unsigned int uiLigne = 0; uiLigne < uiT2DNbLignes; uiLigne++){
-            pT2DContenu[uiLigne] = new T[uiT2DNbColonnes];
-            for(unsigned int uiColonne = 0; uiColonne <= uiT2DNbColonnes; uiColonne++){
-                pT2DContenu[uiLigne][uiColonne] = T2DTab.T2DLireContenu()[uiLigne][uiColonne];
-            }
-        }
-        return *this;
-    }
-
-    
+    Ctableau2D<T> operator=(Ctableau2D<T> &T2DTab);
 };
+#include "Ctableau2D.cpp"
 #endif /* Ctableau2D_hpp */
