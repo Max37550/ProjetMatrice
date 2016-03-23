4afeff3406e5240204e580589a6c389f59e9cb3b
Max37550 <maxime.peralez@hotmail.fr>
2016-03-23 17:58:40 +0100

    Added files via upload

--- Ctableau2D.cpp
+++ Ctableau2D.cpp
@@ -8,6 +8,11 @@
 
 #include "Ctableau2D.hpp"
 
+/**
+ * Constructeur par défaut de la classe Ctableau2D : Met le nombre de lignes et colonnes à 0 et le tableau à NULL
+ * @param void
+ * @exception aucune
+ */
 template <class T>
 Ctableau2D<T>::Ctableau2D(){
     uiT2DNbColonnes = 0;
@@ -15,6 +20,11 @@ Ctableau2D<T>::Ctableau2D(){
     pT2DContenu = NULL;
 }
 
+/**
+ * Constructeur de recopie de la classe Ctableau2D : Recopie l'objet de type Ctableau2D passé en paramètre dans un nouvel objet
+ * @param T2DTab : l'objet Ctableau2D à recopier
+ * @exception aucune
+ */
 template <class T>
 Ctableau2D<T>::Ctableau2D(Ctableau2D<T> &T2DTab){
     uiT2DNbLignes = T2DTab.T2DLireNbLignes();
@@ -29,6 +39,11 @@ Ctableau2D<T>::Ctableau2D(Ctableau2D<T> &T2DTab){
     }
 }
 
+/**
+ * Constructeur à 3 arguments de la classe Ctableau2D
+ * @param nbLignes : le nombre de ligne de la matrice, nbColonnes : le nombre de colonnes, pContenu : le tableau
+ * @exception Le tableau doit avoir nbLignes lignes et nbColonnes colonnes
+ */
 template <class T>
 Ctableau2D<T>::Ctableau2D(unsigned int nbLignes, unsigned int nbColonnes,T **pContenu){
     uiT2DNbLignes = nbLignes;
@@ -43,26 +58,55 @@ Ctableau2D<T>::Ctableau2D(unsigned int nbLignes, unsigned int nbColonnes,T **pCo
     }
 }
 
+/**
+ * Destructeur par défaut de la classe Ctableau2D
+ * @param void
+ * @exception aucune
+ */
 template <class T>
 Ctableau2D<T>::~Ctableau2D(){
     
 }
 
+/**
+ * Méthode qui retourne le nombre de lignes d'un Ctableau2D
+ * @param void
+ * @returns le nombre de lignes de type unsigned int
+ * @exception aucune
+ */
 template <class T>
 unsigned int Ctableau2D<T>::T2DLireNbLignes(){
     return uiT2DNbLignes;
 }
 
+/**
+ * Méthode qui retourne le nombre de colonnes d'un Ctableau2D
+ * @param void
+ * @returns le nombre de colonnes de type unsigned int
+ * @exception aucune
+ */
 template <class T>
 unsigned int Ctableau2D<T>::T2DLireNbColonnes(){
     return uiT2DNbColonnes;
 }
 
+/**
+ * Méthode qui retourne le contenu du Ctableau2D
+ * @param void
+ * @returns un tableau à 2 dimensions de type <T>
+ * @exception aucune
+ */
 template <class T>
 T** Ctableau2D<T>::T2DLireContenu(){
     return pT2DContenu;
 }
 
+/**
+ * Méthode qui affiche un Ctableau2D dans la console
+ * @param void
+ * @returns void
+ * @exception aucune
+ */
 template <class T>
 void Ctableau2D<T>::T2DAffichage(){
     for(int i = 0;i<uiT2DNbLignes;i++){
@@ -73,6 +117,12 @@ void Ctableau2D<T>::T2DAffichage(){
     }
 }
 
+/**
+ * Surcharge de l'opérateur = : Permet d'affectionner un Ctableau2D à un autre
+ * @param T2DTab, un objet de type Ctableau2D
+ * @returns un objet de type Ctableau2D
+ * @exception aucune
+ */
 template <class T>
 Ctableau2D<T> Ctableau2D<T>::operator=(Ctableau2D<T> &T2DTab){
     uiT2DNbLignes = T2DTab.T2DLireNbLignes();
