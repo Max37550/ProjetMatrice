6be36fcb19b717cba0e38b2cf4d8a224b535e1a5
polytechI8 <arthur.crocquevieille@gmail.com>
2016-03-26 09:40:35 +0100

    Gestionmatrice
    J'ai fais ça en utilisant le modèle que tu disais et ça à l'air de marcher, j'ai juste pas gérer la mémoire encore
    double
    2
    4
    1 2 3
    4 5 6

---
+++ CGestionFicherMatrice.cpp
@@ -0,0 +1,9 @@
+//
+//  CGestionFicherMatrice.cpp
+//  projetMatrice
+//
+//  Created by Arthur Crocquevieille on 25/03/2016.
+//  Copyright © 2016 Arthur Crocquevieille. All rights reserved.
+//
+
+//#include "CGestionFicherMatrice.hpp"
---
+++ CGestionFicherMatrice.hpp
@@ -0,0 +1,49 @@
+//
+//  CGestionFicherMatrice.hpp
+//  projetMatrice
+//
+//  Created by Arthur Crocquevieille on 25/03/2016.
+//  Copyright © 2016 Arthur Crocquevieille. All rights reserved.
+//
+
+#ifndef CGestionFicherMatrice_hpp
+#define CGestionFicherMatrice_hpp
+
+#include <stdio.h>
+#include "Cmatrice.hpp"
+#include "Cexception.hpp"
+#include <iostream>
+#include <fstream>
+class CGestionFichierMatrice {
+    //Attributs
+    //Constructeur
+    public:
+    CGestionFichierMatrice();
+    static Cmatrice<double> GFMLireMatriceDansFichier(char * chemin);
+};
+
+Cmatrice<double> CGestionFichierMatrice::GFMLireMatriceDansFichier(char * chemin){
+    std::ifstream f(chemin, std::ios::in);
+    if(f){
+        char ctype[1024];
+        unsigned int uiNbLignes,uiNbColonnes;
+        f >> ctype >> uiNbLignes >> uiNbColonnes;
+        double **dtab = (double**)malloc(uiNbLignes*sizeof(double*));
+        for(unsigned int uiBoucle = 0;uiBoucle < uiNbLignes;uiBoucle++){
+            dtab[uiBoucle] = (double*)malloc(uiNbColonnes*sizeof(double));
+            for(unsigned int uiBoucle2 = 0;uiBoucle2 < uiNbColonnes;uiBoucle2++){
+                f >> dtab[uiBoucle][uiBoucle2];
+            }
+        }
+        //VERIFIER si c'est un double
+        
+        f.close();
+        Cmatrice<double> m(uiNbLignes,uiNbColonnes,dtab);
+        return m;
+    }else{
+        Cexception e(1);
+        throw e;
+    }
+}
+
+#endif /* CGestionFicherMatrice_hpp */
